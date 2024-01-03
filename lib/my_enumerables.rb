8d6f6d3c4a6920e8e6066ab5621ad9483ffa8897
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2024-01-03 23:16:47 +0530

    Define Enumerable#my_each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,16 @@
 module Enumerable
+  def my_each_with_index
+    return self unless block_given?
 
+    i = 0
+    enum_length = self.length
+    while i < enum_length
+      i += 1
+      yield(self[i-1], (i - 1))
+    end
+
+    self
+  end
 end
 
 class Array
